adf9c0ac8d1d87252b156d0db9a5e63e7fddcd51
Aleksei Obukhov <alexey.obuhov@gmail.com>
2023-05-04 14:18:37 +0300

    implementing canvas

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 
 import './rxjs-lessons';
+import './canvas';
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
--- src/rxjs-lessons.ts
+++ src/rxjs-lessons.ts
@@ -1,11 +1,36 @@
 import {debounce, debounceTime, distinctUntilChanged, fromEvent, map, Observable} from "rxjs";
 
-const search$ = fromEvent<Event>(
-  document.getElementById('searchId')!,
-  'input'
-);
+const search$ = new Observable<Event>(observer => {
 
-search$.pipe(
+  const search = document.getElementById('searchId');
+  const stop = document.getElementById('stop');
+
+    if (!search || !stop) {
+        observer.error('element dont exist on the page');
+        return;
+      }
+
+    const onSearch = (event: Event) => {
+      console.log(123);
+      observer.next(event);
+    };
+
+    const onStop = (event:Event)=> {
+      observer.complete();
+      clear();
+    };
+
+    search.addEventListener('input', onSearch);
+    stop.addEventListener('click', onStop);
+
+    const clear = () => {
+      search.removeEventListener('input', onSearch);
+      search.removeEventListener('click', onStop);
+    };
+
+  });
+
+const searchSubscription = search$.pipe(
   map(event=> {return (event.target as HTMLInputElement).value}),
   debounceTime(500),
   map(value => {return value.length> 3 ? value: '';}),
@@ -13,4 +38,9 @@ search$.pipe(
 ).subscribe(value => {
   console.log(value)
 });
+
+setTimeout(() => {
+  console.log('unsubcribed');
+  searchSubscription.unsubscribe();
+}, 10000);
 
